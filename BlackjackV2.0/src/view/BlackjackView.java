d9a567cdd8664454da38af9087784f1c468cdb1e
henry <henry.lin@uconn.edu>
2014-05-13 23:13:07 -0400

    initial commit

---
+++ BlackjackV2.0/src/view/BlackjackView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class BlackjackView extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public BlackjackView() {
+        
+    }
+
+}
