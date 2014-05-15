cc3eb451be2b9884536d688eabaddf49d4f883ca
henry <henry.lin@uconn.edu>
2014-05-15 19:33:33 -0400

    moved ui-building to constructor of the view

--- BlackjackV2.0/src/Main.java
+++ BlackjackV2.0/src/Main.java
@@ -22,42 +22,7 @@ public class Main {
     }
 
     protected static void createAndShowGui() {
-        JPanel panelNorth = new JPanel();
-        JPanel panelSouth = new JPanel();
-        JPanel panelEast  = new JPanel();
-        JPanel panelCenter = new JPanel();
-        JButton centerButton = new JButton("center");
-        JButton hitButton = new JButton("Hit");
-        JButton doubleButton = new JButton("Double Down");
-        JButton splitButton = new JButton("Split");
-        JButton surrenderButton = new JButton("Surrender");
-        JButton stayButton = new JButton("Stay");
-        JButton eastB = new JButton("East");
-        JButton northB = new JButton("North");
-        JButton westB = new JButton("West");
-        
-        panelCenter.add(centerButton);
-        panelSouth.add(hitButton);
-        panelSouth.add(doubleButton);
-        panelSouth.add(splitButton);
-        panelSouth.add(surrenderButton);
-        panelSouth.add(stayButton);
-        panelNorth.add(northB);
-        panelEast.add(eastB);
-        
-//        panelSouth.setBackground(Color.BLUE);
-        panelSouth.setBorder(BorderFactory.createRaisedBevelBorder());
-        panelEast.setBorder(BorderFactory.createRaisedBevelBorder());
-        panelNorth.setBorder(BorderFactory.createRaisedBevelBorder());
-        
         JFrame blackjackView = new BlackjackView(); 
-        blackjackView.setLayout(new MigLayout());
-
-        blackjackView.add(panelNorth, "dock north");
-        blackjackView.add(panelCenter, "height 500!, width 500!");
-        blackjackView.add(panelSouth, "dock south");
-        blackjackView.add(panelEast,   "dock east, width 100!");
-        
         blackjackView.pack();
         blackjackView.setVisible(true);
     }
--- BlackjackV2.0/src/view/BlackjackView.java
+++ BlackjackV2.0/src/view/BlackjackView.java
@@ -1,13 +1,77 @@
 package view;
 
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.miginfocom.swing.MigLayout;
 
 public class BlackjackView extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
     public BlackjackView() {
+        JPanel panelN           = new JPanel();
+        JPanel panelNE          = new JPanel();
+        JPanel panelNW          = new JPanel();
+        JPanel panelS           = new JPanel();
+        JPanel panelE           = new JPanel();
+        JPanel panelC           = new JPanel();
+        JPanel seat1Panel       = new JPanel();
+        JPanel seat2Panel       = new JPanel();
+        JPanel seat3Panel       = new JPanel();
+        JPanel seat4Panel       = new JPanel();
+        JPanel seat5Panel       = new JPanel();
+        JPanel seat6Panel       = new JPanel();
+        JButton hitButton       = new JButton("Hit");
+        JButton doubleButton    = new JButton("Double Down");
+        JButton splitButton     = new JButton("Split");
+        JButton surrenderButton = new JButton("Surrender");
+        JButton stayButton      = new JButton("Stay");
+        JButton dealHandsButton = new JButton("Deal Hands");
+        JButton shuffleButton   = new JButton("Shuffle");
         
+        panelN.setLayout(new MigLayout("", "[grow]", "[]"));
+        panelNE.setLayout(new BoxLayout(panelNE, BoxLayout.Y_AXIS));
+        panelNE.add(dealHandsButton);
+        panelNE.add(shuffleButton);
+        panelNW.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        panelN.add(panelNW, "dock west, width 600!");
+        panelN.add(panelNE, "dock east");
+        
+        seat1Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        seat2Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        seat3Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        seat4Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        seat5Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        seat6Panel.setBorder(BorderFactory.createRaisedBevelBorder());
+        panelC.setLayout(new MigLayout());
+        panelC.add(seat1Panel, "width 100!");
+        panelC.add(seat2Panel, "width 100!");
+        panelC.add(seat3Panel, "width 100!");
+        panelC.add(seat4Panel, "width 100!");
+        panelC.add(seat5Panel, "width 100!");
+        panelC.add(seat6Panel, "width 100!");
+        
+        panelS.add(hitButton);
+        panelS.add(doubleButton);
+        panelS.add(splitButton);
+        panelS.add(surrenderButton);
+        panelS.add(stayButton);
+        
+        panelS.setBorder(BorderFactory.createRaisedBevelBorder());
+        panelE.setBorder(BorderFactory.createRaisedBevelBorder());
+        panelN.setBorder(BorderFactory.createRaisedBevelBorder());
+        panelC.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        this.setLayout(new MigLayout());
+        this.add(panelN, "dock north");
+        this.add(panelC, "height 500!, width 700:800:900");
+        this.add(panelS, "dock south");
     }
 
 }
